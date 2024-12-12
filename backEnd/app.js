a4f9a06036b69514a011f82afc04b5a554ed97cd
Jagrit Bharara <jagritbharara12@gmail.com>
2024-12-12 14:18:00 +0530

    Register,Login Implemented

--- backEnd/app.js
+++ backEnd/app.js
@@ -3,13 +3,22 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes')
+const cookieParser = require('cookie-parser');
 
+connectToDb();
 app.use(cors()); 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 // const PORT = 4444;
 app.get('/',(req,res)=>{
     res.send("Lets Begin");
 })
+app.use('/users',userRoutes);
 
 
 module.exports = app;
